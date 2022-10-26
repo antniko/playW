b45cd08e79b6c68161bbdff91e8b56443a386e32
Anton Kolupaiev <antnikol@gmail.com>
2022-10-26 11:59:28 +0300

    new install

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+// @ts-check
+const { test, expect } = require('@playwright/test');
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -14,7 +15,7 @@ test('homepage has Playwright in title and get started link linking to the intro
 
   // Click the get started link.
   await getStarted.click();
-
+  
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
