5dbeef47ce8806d64e95c7179ae84bbbba109aa0
Anton Kolupaiev <antnikol@gmail.com>
2022-10-28 08:37:30 +0300

    111

---
+++ tests/mytest.spec.js
@@ -0,0 +1,14 @@
+
+const {test, expect} = require("@playwright/test")
+//import {test, expect} = require ('@playwright/test')
+
+const {hello, helloworld} = require('./demo/hello')
+//import {hello, helloworld} from './demo/hello'
+
+//console.log (hello());
+//console.log (helloworld());
+
+test('My test', async ({page}) => {
+  await page.goto('https://google.com');
+  await expect(page).toHaveTitle('Google')
+})
