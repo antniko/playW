1880960df912464278e8e17fe9c8ecd2f465aa9c
Anton Kolupaiev <antnikol@gmail.com>
2022-10-28 22:23:19 +0300

    login_check_test

---
+++ tests/check_login.spec.js
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test'
+
+test('Login test 1', async({page}) => {
+
+  await page.goto('https://demo.applitools.com/')
+  //await page.pause()
+  await page.locator('internal:attr=[placeholder="Enter your username"i]').fill('JackGomez')
+  await page.locator('internal:attr=[placeholder="Enter your password"i]').fill('password')
+  await page.waitForSelector('role=link[name="Sign in"]',{timeout: 5000})
+  await page.locator('role=link[name="Sign in"]').click()
+  await page.locator('text=Jack Gomez')
+
+})
